c0088862826828fd72e5b864962ed9c72752f7e5
Robert Fernandez <robertf@lastyard.com>
2021-11-16 16:13:53 +0800

    Fix deprecation warning for default_app_config in Django > 3.2

--- hijack_admin/__init__.py
+++ hijack_admin/__init__.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
 __version__ = '2.1.10'  # pragma: no cover
 
-default_app_config = 'hijack_admin.apps.HijackAdminConfig'
+import django
+
+if django.VERSION < (3, 2):
+    default_app_config = 'hijack_admin.apps.HijackAdminConfig'
